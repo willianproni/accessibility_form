fa75e9c355c9a2aaa0ff7237a49864f541ee5a6b
willianproni <pronifwillian@gmail.com>
2025-07-09 07:58:22 -0300

    Feature | Created button variants

---
+++ app/components/Button.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from "react";
+import { tv, VariantProps } from "tailwind-variants";
+
+const button = tv({
+  base: [
+    "rounded-lg px-4 py-2 text-sm font-semibold outline-none shadow-sm",
+    "focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500",
+    "active:opacity-80",
+  ],
+  variants: {
+    variant: {
+      primary: "bg-violet-600 text-white hover:bg-violet-700",
+      ghost: "rounded-md px-2 hover:bg-zinc-200 shadow-none",
+      outline: "bg-white text-zinc-700 border-zinc-300 hover:bg-zinc-50",
+    },
+  },
+
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+export type ButtonProps = ComponentProps<"button"> &
+  VariantProps<typeof button>;
+
+export const Button = ({ variant, ...props }: ButtonProps) => {
+  return <button {...props} className={button({ variant })} />;
+};
