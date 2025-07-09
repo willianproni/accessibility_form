31147159d1ca1e66a1b7ef147f071fdad2be7508
willianproni <pronifwillian@gmail.com>
2025-07-09 07:58:50 -0300

    Refactor | Integration button in components and page

--- app/components/Form/FileInput/FileList.tsx
+++ app/components/Form/FileInput/FileList.tsx
@@ -4,12 +4,11 @@ import { Trash2, UploadCloud } from "lucide-react";
 import { useFileInput } from "./Root";
 import { formatBytes } from "@/app/utils/format-bytes";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { Button } from "../../Button";
 
 export const FileList = () => {
   const { files } = useFileInput();
- const [parent] = useAutoAnimate({
-
- })
+  const [parent] = useAutoAnimate({});
   return (
     <div ref={parent} className="mt-4 flex flex-col space-y-3">
       {files.map((files) => (
@@ -43,12 +42,9 @@ export const FileList = () => {
             </div>
           </div>
 
-          <button
-            type="button"
-            className="ml-auto p-2  hover:bg-zinc-200 rounded"
-          >
+          <Button type="button" variant="ghost">
             <Trash2 className="h-5 w-5 text-zinc-500" />
-          </button>
+          </Button>
         </div>
       ))}
     </div>
